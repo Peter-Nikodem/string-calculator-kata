b8097b7f056d5bb762d5aa7ef07eb537c2c2bf3b
Peter Nikodem <peternikodemjr@gmail.com>
2016-03-22 14:12:02 +0100

    Refactor after the uknown amount of numbers test.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,21 +3,14 @@
  */
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.isEmpty()) return 0;
-        String[] separatedNumbers = numbers.split(",");
-        if (separatedNumbers.length==1){
-            return parseToInt(separatedNumbers[0]);
-        } else {
-            int sum=0;
-            for (String number:separatedNumbers) {
-                sum += parseToInt(number);
-            }
-            return sum;
+        if (numbers.isEmpty()) {
+            return 0;
         }
-
-    }
-
-    private int parseToInt(String s) {
-        return Integer.parseInt(s);
+        int sum = 0;
+        String[] separatedNumbers = numbers.split(",");
+        for (String number : separatedNumbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
